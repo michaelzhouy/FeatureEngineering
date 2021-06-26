58133f10ab024659139d5b9df4d751013535e442
Michaelzhouy <15602409303@163.com>
2021-06-26 19:40:46 +0800

    增加模型

---
+++ 06-Model/01-ML/01-lgb/02-regression.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# Time   : 2021/6/26 19:32
+# Author : Michael_Zhouy
+import numpy as np
+from sklearn.metrics import mean_squared_log_error
+
+
+def rmsle(y_hat, data):
+    y_true = data.get_label()
+    y_hat = np.where(y_hat < 0, 1, y_hat)
+    y_true = np.where(y_true < 0, 1, y_hat)
+    res = np.sqrt(mean_squared_log_error(y_true, y_hat))
+    return 'rmsle', res, True
+
+
+# regression
+params = {
+    'boosting_type': 'gbdt',
+    'objective': 'regression',
+    'metric': 'mae',
+    'learning_rate': 0.05,
+    'verbose': -1,
+    'seed': 2020
+}
