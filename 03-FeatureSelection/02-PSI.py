2fb654464e9874cfc6bcad0da9057521d89e830d
zhouyang <41115930+michaelzhouy@users.noreply.github.com>
2021-07-19 10:13:31 +0800

    PSI串行代码

--- 03-FeatureSelection/02-PSI.py
+++ 03-FeatureSelection/02-PSI.py
@@ -59,3 +59,39 @@ def get_psi(c, x_train, x_test):
     except:
         print(c, 'error')
     return psi_res  #, psi_dict
+
+
+def get_psi(f_cols, x_train, x_test):
+    psi_res = pd.DataFrame()
+    psi_dict = {}
+    for c in tqdm(f_cols):
+        try:
+            t_train = x_train[c].fillna(-998)
+            t_test = x_test[c].fillna(-998)
+            # 获取切分点
+            bins = []
+            for i in np.arange(0,1.1,0.2):
+                bins.append(t_train.quantile(i))
+            bins = sorted(set(bins))
+            bins[0] = -np.inf
+            bins[-1] = np.inf
+            # 计算psi
+            t_psi = pd.DataFrame()
+            t_psi['train'] = pd.cut(t_train, bins).value_counts().sort_index()
+            t_psi['test'] = pd.cut(t_test, bins).value_counts()
+            t_psi.index = [str(x) for x in t_psi.index]
+            t_psi.loc['总计', :] = t_psi.sum()
+            t_psi['train_rate'] = t_psi['train'] / t_psi.loc['总计', 'train']
+            t_psi['test_rate'] = t_psi['test'] / t_psi.loc['总计', 'test']
+            t_psi['psi'] = (t_psi['test_rate'] - t_psi['train_rate']) * (np.log(t_psi['test_rate']) - np.log(t_psi['train_rate']))
+            t_psi.loc['总计', 'psi'] = t_psi['psi'].sum()
+            t_psi.index.name = c
+            # 汇总
+            t_res = pd.DataFrame([[c, t_psi.loc['总计', 'psi']]],
+                                 columns=['变量名', 'PSI'])
+            psi_res = pd.concat([psi_res, t_res])
+            psi_dict[c] = t_psi
+            print(c, 'done')
+        except:
+            print(c, 'error')
+    return psi_res, psi_dict
