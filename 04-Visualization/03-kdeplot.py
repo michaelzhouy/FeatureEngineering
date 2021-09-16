b3f642ad141b5f54a07b6fc4cbcf04fb16e40fc7
zhouyang <41115930+michaelzhouy@users.noreply.github.com>
2021-09-16 12:20:14 +0800

    Update 03-kdeplot.py

--- 04-Visualization/03-kdeplot.py
+++ 04-Visualization/03-kdeplot.py
@@ -14,3 +14,15 @@ def plot(df1, df2, df3, x):
     sns.kdeplot(data=df3, x=x)
     plt.legend(['origin', 'sample', 'new'])
     plt.show()
+
+
+def multi_plot(df1, df2, feats):
+    rows, cols = 5, 4
+    plt.figure(figsize=(4 * rows, 4 * cols))
+    i = 0
+    for col in feats:
+        i += 1
+        ax = plt.subplot(rows, cols, i)
+        sns.kdeplot(df1[col], ax=ax)
+        sns.kdeplot(df2[col], ax=ax)
+    plt.show()
